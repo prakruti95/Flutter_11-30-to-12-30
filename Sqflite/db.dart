0782c12cd75e8e6eb30aa7178de1543107aff37e
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-05-26 12:26:46 +0530

    commit

---
+++ Sqflite/db.dart
@@ -0,0 +1,37 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+class MyDb
+{
+  //database object
+  late Database db;
+
+  //when i will call open
+  Future open() async
+  {
+    var dbpath = await getDatabasesPath();//path get
+    String path = join(dbpath,'flutterbatch.db');//database name
+    print("ABCD: $path");
+
+    db = await openDatabase
+      (
+        path,
+        version: 1,
+        onCreate:(Database db, int version) async
+        {
+          await db.execute('''
+
+                    CREATE TABLE IF NOT EXISTS students( 
+                          id primary key,
+                          name varchar(255) not null,
+                          email varchar(255) not null,
+                          password varchar(255) not null
+                      );
+
+                      //create more table here
+                  
+                  ''');
+
+          print('Table Created');
+        });
+  }
+}
