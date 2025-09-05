0c8162e6bbe91fefd3295b8aa6dede46986e6527
Prakruti_Tops <109515514+prakrutitops@users.noreply.github.com>
2025-09-05 19:03:22 +0530

    commit

--- Project/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:project1/user/splashscreen/SplashScreen.dart';
-
-void main()
-{
-  runApp(MaterialApp(home:SplashScreen(),debugShowCheckedModeBanner: false,));
-}
---
+++ Project/new/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:project2/user/splashscreen/SplashScreen.dart';
+
+
+void main()
+{
+  runApp
+    (
+      MaterialApp
+      (
+          home:SplashScreen()
+      )
+    );
+}
